b55797ff736e7890a01c067fdda231af1e3b5b2e
LumisDev <hiba.chabiene12@gmail.com>
2025-11-15 16:07:12 +0100

    Now using Nuitka is crap

--- compile.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-subprocess.run(['python', "-m", 'nuitka', '--follow-imports', "--include-data-dir=train_assets=./train_assets", "--include-data-dir=eval_assets=./eval_assets","--mode=app-dist", "--clang", "--enable-plugins=matplotlib", 'main.py'])
